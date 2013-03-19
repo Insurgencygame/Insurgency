87b1c2c5914857191aefce6c342d441e3b479b18
Zealot <Pete@128itlove.com>
2013-03-19 23:34:15 +0800

    1st commit 1.04

---
+++ Insurgency_1.04.sdd/modinfo.lua
@@ -0,0 +1,10 @@
+return {
+  name='Insurgency',
+  description='Insurgency a game of stealth and ranged warfare.',
+  shortname='Ins',
+  version='V1.04',
+  mutator='Official',
+  game='Total Annihilation',
+  shortGame='TA',
+  modtype=1,
+}
