4a56b4e43fcf3a2f4260e066b7a5382dc9ee4b1b
Insurgencygame <Pete@128bitlove.com>
2013-04-04 00:00:51 +0800

    1.5 speed stealth

---
+++ Insurgency_1.05.sdd/modinfo.lua
@@ -0,0 +1,10 @@
+return {
+  name='Insurgency',
+  description='Insurgency a game of stealth and ranged warfare.',
+  shortname='Ins',
+  version='V1.05',
+  mutator='Official',
+  game='Total Annihilation',
+  shortGame=' hTA',
+  modtype=1,
+}
