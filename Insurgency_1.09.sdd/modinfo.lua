e5b16f8828715fe7374123b5bb9ddf700f113bb6
Pete <Pete@128bitlove.com>
2013-07-19 15:13:55 +0800

    tweaks

--- Insurgency_1.09.sdd/modinfo.lua
+++ Insurgency_1.09.sdd/modinfo.lua
@@ -2,7 +2,7 @@ return {
   name='Insurgency',
   description='Insurgency a game of stealth and ranged warfare.',
   shortname='Ins',
-  version='V1.08',
+  version='$VERSION',
   mutator='Official',
   game='Total Annihilation',
   shortGame='ins',
